6d077490d22e4b62177baa29ffa2e21d1ae23cd1
OSCAR MARTIN <elmao79@gmail.com>
2018-06-03 20:49:39 +0200

    Demo file added

--- compile.js
+++ compile.js
@@ -1,9 +1,12 @@
 
-var compressor = require('node-minify');
+const compressor = require('node-minify');
+const Fs = require('fs');
 
 compressor.minify({
     compressor: 'gcc',
     input: 'src/mcparallax.js',
     output: 'dist/mcparallax.min.js',
-    callback: function(err, min) {}
+    callback: function(err, min) {
+        Fs.copyFileSync('dist/mcparallax.min.js', 'demo/mcparallax.min.js');
+    }
 });
